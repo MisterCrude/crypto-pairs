5141ac8f61750eda68cbbd8693bd74dee9a491f6
calamberia <calamberia@gmail.com>
2018-01-06 19:51:41 +0100

    leave gray color item after mouseOver evernt

--- src/components/elements/searchInputSuggestBox/SearchInputSuggestBox.jsx
+++ src/components/elements/searchInputSuggestBox/SearchInputSuggestBox.jsx
@@ -36,20 +36,14 @@ class SearchInputSuggestBox extends React.Component {
         this.props.setSelectedItem(coinName, fullCurrencyName)
     };
 
-    hoverAnimation = (event, isOverEvent) => {
-        if (!event) {
+    setHoverStyle = (itemNumber) => {
+        if (!itemNumber && itemNumber != 0) {
             return;
         }
-        let item = (event.target) ? event.target : event;
-        let animationOptions = {
-            easing: 'ease-in-out',
-            iterations: '1',
-            fill: 'forwards',
-            duration: 300,
-        };
-        let backgroundColor = (isOverEvent) ? [variables.white, variables.lightGay] : [variables.lightGay, variables.white];
 
-        item.animate({backgroundColor}, animationOptions);
+        this.listItems.forEach((item, index )=> {
+            item.style.backgroundColor = (itemNumber === index) ? variables.lightGay : variables.white;
+        });
     };
 
     moveItemHighlight = (moveDirection) => {
@@ -62,32 +56,30 @@ class SearchInputSuggestBox extends React.Component {
         if (moveDirection === 'ArrowUp') {
             this.setState(prevState => {
                 let newState = (prevState.activeItem <= 0) ? 0 : prevState.activeItem-1;
-                return {activeItem: newState}
+                return { activeItem: newState };
             });
         } else if (moveDirection === 'ArrowDown') {
             this.setState(prevState => {
                 let newState = ((prevState.activeItem+1) >= this.listItems.length) ? prevState.activeItem : prevState.activeItem+1;
-                return  {activeItem: newState}
+                return  { activeItem: newState };
             });
         }
         // Set styles to current item
         setTimeout(() => {
-            this.hoverAnimation(this.listItems[this.state.activeItem], true)
+            this.setHoverStyle(this.state.activeItem);
         }, 10);
     };
 
     componentWillReceiveProps(nextProps) {
-        console.log(nextProps);
-
         this.calcList(this.props.items, nextProps.inputValue);
         this.moveItemHighlight(nextProps.keyboardButtonsActive);
 
         // Highlight first item by default
-        if (this.listItems.length) {
-            setTimeout(() => {
-                this.hoverAnimation(this.listItems[this.state.activeItem], true)
-            }, 10);
-        }
+        // if (this.listItems.length) {
+        //     setTimeout(() => {
+        //         this.setHoverStyle(this.state.activeItem)
+        //     }, 10);
+        // }
     }
 
     render() {
@@ -97,16 +89,13 @@ class SearchInputSuggestBox extends React.Component {
 
         return(
             <ul style={{...SearchInputSuggestBoxStyles.list, display: (suggestBoxStatus) ? 'block' : 'none'}}>
-                {this.state.itemsForShowing.map((item, i) =>
+                {this.state.itemsForShowing.map((item, index) =>
                     <li
                         style={{...SearchInputSuggestBoxStyles.item, cursor: 'pointer'}}
                         key={HelpersFoo.getRandomNumber()}
-                        ref={(el => this.listItems[i] = el)}
+                        ref={(el => this.listItems[index] = el)}
                         onClick={() => this.setSelectedItem(item.code, `${item.code} - ${item.name}`)}
-                        onFocus={(e) => this.hoverAnimation(e, true)}
-                        onBlur={(e) => this.hoverAnimation(e, false)}
-                        onMouseOver={(e) => this.hoverAnimation(e, true)}
-                        onMouseLeave={(e) => this.hoverAnimation(e, false)} >{item.code} - {item.name}</li>)}
+                        onMouseOver={() => this.setHoverStyle(index)} >{item.code} - {item.name}</li>)}
 
                     {/* No results */}
                     {this.state.itemsForShowing.length == 0 &&
