c9f9d70b72128d34a0618c0eac58e60767573cc0
calamberia <calamberia@gmail.com>
2019-11-01 15:17:39 +0100

    Updates

---
+++ precache-manifest.634dfdeb8feb9446fa9cc1be552303be.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584abbd01c7190215a3501c5840cc55c",
+    "url": "/crypto-pairs/index.html"
+  },
+  {
+    "revision": "853a4fb080a0419ec0a5",
+    "url": "/crypto-pairs/static/css/2.063bdab7.chunk.css"
+  },
+  {
+    "revision": "d15c3bd2542bc9229971",
+    "url": "/crypto-pairs/static/css/main.21255cdc.chunk.css"
+  },
+  {
+    "revision": "853a4fb080a0419ec0a5",
+    "url": "/crypto-pairs/static/js/2.1a658f56.chunk.js"
+  },
+  {
+    "revision": "d15c3bd2542bc9229971",
+    "url": "/crypto-pairs/static/js/main.d25d0b58.chunk.js"
+  },
+  {
+    "revision": "803ad5939b46cd684728",
+    "url": "/crypto-pairs/static/js/runtime-main.810da60a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/crypto-pairs/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/crypto-pairs/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/crypto-pairs/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/crypto-pairs/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/crypto-pairs/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "53155019311fc699a35d07d581a1890f",
+    "url": "/crypto-pairs/static/media/logo.53155019.svg"
+  },
+  {
+    "revision": "ebc94bc106f5b0bc7a96d57864318ba0",
+    "url": "/crypto-pairs/static/media/main-bg.ebc94bc1.png"
+  }
+]);
